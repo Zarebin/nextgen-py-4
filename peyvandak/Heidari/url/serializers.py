97499e4d2838c68b480a8ec2b7a568cd93515c0e
milad.h9999 <milad.h9999@yahoo.com>
2022-09-02 09:57:06 +0430

    Add my project folder

---
+++ peyvandak/Heidari/url/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from .models import URLMap
+
+class URLMapSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = URLMap
+        fields = ('long_url', )
