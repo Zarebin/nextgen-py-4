e25c088d7e6017e167c88040072fdcbf236e7ca2
TahooraMajlesi <tahooramajlesi@yahoo.com>
2022-09-03 00:53:10 +0430

    Delete peyvandak/Majlesi/django-docker-compose-url-shortener/urlshortener_api directory

--- peyvandak/Majlesi/django-docker-compose-url-shortener/urlshortener_api/api/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Shortener
-from .models import VisitUrl
-
-
-# Register your models here.
-admin.site.register(Shortener)
-admin.site.register(VisitUrl)
