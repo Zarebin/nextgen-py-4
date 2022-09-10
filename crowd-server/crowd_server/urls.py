b7ded41d5b888d625ead133f8fbe0618f191ef98
amin-rami <ma.rami@yahoo.com>
2022-09-11 00:35:18 +0430

    add sentiment app

--- crowd-server/crowd_server/urls.py
+++ crowd-server/crowd_server/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('api/food_fact', include('crowd_server.apps.food_fact.urls')),
     path('api/image_caption', include('crowd_server.apps.image_caption.urls')),
     path('api/food_labeler', include('crowd_server.apps.food_labeler.urls')),
+    path('api/sentiment', include('crowd_server.apps.sentiment.urls'))
 ]
 
 
