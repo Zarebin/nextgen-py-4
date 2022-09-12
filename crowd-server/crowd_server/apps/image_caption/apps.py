764e02ce2e71c10a039f3599d12869c8a7de7d5e
TahooraMajlesi <tahooramajlesi@yahoo.com>
2022-09-12 17:11:07 +0430

    Delete crowd-server directory

--- crowd-server/crowd_server/apps/image_caption/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class ImageCaptionConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "crowd_server.apps.image_caption"
-    
--- crowd-server/crowd_server/apps/image_caption/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-    path('', views.ImageCaption.as_view(), name="image_caption")
-]
